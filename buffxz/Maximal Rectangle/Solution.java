c8d5e01764c3038e364a0f5133db1149954d2d46
buffxz <buffxz@gmail.com>
2014-07-25 23:42:35 -0700

    My daily push

---
+++ buffxz/Maximal Rectangle/Solution.java
@@ -0,0 +1,77 @@
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int row = matrix.length;
+        if (row == 0) return 0;
+        int col = matrix[0].length;
+        
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < row; i++) {
+            int[] height = new int[col];
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    int count = 1;
+                    int curRow = i;
+                    while(curRow - 1 >= 0 && matrix[curRow-1][j] == '1') {
+                        count++;
+                        curRow--;
+                    }
+                    height[j] = count;
+                }
+            }
+            max = Math.max(max, largestRectangleArea(height));
+        }
+        return max;
+    }
+    
+        public static int largestRectangleArea(int[] height) {  
+        int len = height.length;
+        if (len == 0) return 0;
+         
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(-1);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (stack.peek() == -1) {
+            	stack.push(i);
+            	continue;
+            }
+            int peekIndex = stack.peek();
+            int curNum = height[i];
+            if (height[peekIndex] <= curNum) {
+                stack.push(i);
+            } else {
+                // The special one
+                int iniINDEX = stack.pop();
+                int comIndex = stack.peek();
+                while (comIndex != -1 && height[iniINDEX] == height[comIndex]) {
+                	comIndex--;
+                }
+                max = Math.max(max, height[iniINDEX] * (iniINDEX - comIndex));
+                int prevIndex = iniINDEX;
+                while(stack.peek() > -1 && height[stack.peek()] > curNum) {
+                    int curIndex = stack.pop();
+                    max = Math.max(max, height[curIndex] * (iniINDEX - stack.peek()));
+                    prevIndex = curIndex;
+                }
+                stack.push(i);
+            }
+        }
+        
+        if (!stack.isEmpty()) {
+            int iniINDEX = stack.pop();
+            int comIndex = stack.peek();
+            while (comIndex != -1 && height[iniINDEX] == height[comIndex]) {
+            	comIndex--;
+            }
+            max = Math.max(max, height[iniINDEX] * (iniINDEX - comIndex));
+            int prevIndex = iniINDEX;
+            while (stack.peek() > -1) {
+                int curIndex = stack.pop();
+                max = Math.max(max, height[curIndex] * (iniINDEX - stack.peek()));
+                prevIndex = curIndex;
+            }
+        }
+        
+        return max;
+    }
+}
