dd52d1525d96b43da693dbf5acf94b5ead893ddb
buffxz <buffxz@gmail.com>
2014-08-04 21:45:49 -0700

    My daily push

---
+++ buffxz/Balanced Binary Tree /Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) >= 0;
+    }
+    
+    public int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        
+        if (left < 0 || right < 0 || Math.abs(left - right) > 1) return -1;
+        return Math.max(left, right) + 1;
+    }
+}
